7731ed477c8b38ed69ca865dfb9957d88098da2b
daichi <kabata0210@icloud.com>
2025-04-18 21:56:46 +0900

    for文

--- lesson2.go
+++ lesson2.go
@@ -11,6 +11,44 @@ func by2(num int) string {
 }
 
 func main() {
+	for i := 0; i < 10; i++ {
+		if i == 3 {
+			fmt.Println("continue")
+			continue
+		}
+		if i > 5 {
+			fmt.Println("break")
+			break
+		}
+		fmt.Println(i)
+	}
+
+	sum := 1
+	for sum < 10 {
+		sum += sum
+		fmt.Println(sum)
+	}
+	fmt.Println(sum)
+
+	l := []string{"python", "go", "java"}
+	for i := 0; i < len(l); i++ {
+		fmt.Println(i, l[i])
+	}
+	for i, v := range l {
+		fmt.Println(i, v)
+	}
+	for _, v := range l {
+		fmt.Println(v)
+	}
+
+	m := map[string]int{"apple": 100, "banana": 200}
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+	for _, v := range m {
+		fmt.Println(v)
+	}
+	/*
 	num := 9
 	if num%2 == 0 {
 		fmt.Println("by 2")
@@ -38,4 +76,5 @@ func main() {
 	fmt.Println("great2")
 	}
 	// fmt.Println(result2)
+	*/
 }
